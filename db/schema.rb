06f608c4df174ce8a5847796e04ca7ac86a53f85
shunsuke <shun.8000.sf@gmail.com>
2022-09-27 08:28:37 +0900

    Add column 'updatedAt' to Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_232618) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_232817) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "memo_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "createdAt"
+    t.string "updatedAt"
     t.index ["memo_id"], name: "index_comments_on_memo_id"
   end
 
